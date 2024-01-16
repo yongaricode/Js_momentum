6ee0cb58caa58ffc973da721534fe8c3826cf73d
yongaricode <149791764+yongaricode@users.noreply.github.com>
2024-01-16 18:20:16 +0900

    Add files via upload

---
+++ js/greetings.js
@@ -0,0 +1,29 @@
+const LoginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    LoginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    LoginForm.classList.remove(HIDDEN_CLASSNAME);
+    LoginForm.addEventListener("submit", onLoginSubmit);
+} else {
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+    paintGreetings(savedUsername);
+}
